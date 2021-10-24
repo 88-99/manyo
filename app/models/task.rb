306533d84746a4528cf8435a9581ad49c9e80b17
KW <kw@KWnoMacBook-Pro.local>
2021-10-24 20:48:42 +0900

    model specのフォーマットを記述

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
-  # validates :registration_date, presence: true
   # validates :status, presence: true
 end
