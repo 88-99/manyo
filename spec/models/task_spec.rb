eace3dff0a2b9eabd2418d721cfe8d7a1bc0fc25
KW <kw@KWnoMacBook-Pro.local>
2021-10-24 21:14:42 +0900

    タスクの詳細が空の場合、タスクのタイトルと詳細に内容が記載されている場合のmodel speckを記載

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,13 +9,14 @@ describe 'タスクモデル機能', type: :model do
     end
     context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(title: '', content: '失敗テスト')
+        task = Task.new(title: '失敗テスト', content: '')
         expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        # ここに内容を記載する
+        task = Task.new(title: '失敗テスト', content: '失敗テスト')
+        expect(task).to be_valid
       end
     end
   end
