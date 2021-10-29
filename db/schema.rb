c078c91b5492a03f0b90b48b754d2605a71534f6
KW <kw@KWnoMacBook-Pro.local>
2021-10-30 07:46:55 +0900

    開発とテスト環境にstatusカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_10_23_233719) do
     t.string "title", null: false
     t.text "content", null: false
     t.datetime "deadline", null: false
+    t.string "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
