6e7e4688ab20bf64698b3f4bf15066b8fc918776
KW <kw@KWnoMacBook-Pro.local>
2021-11-04 20:15:30 +0900

    ログイン・ログアウト機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
   resources :users, only: %i[new create show]
-  # resources :sessions, only: %i[new create destroy]
+  resources :sessions, only: %i[new create destroy]
 end
