164ebbb71425b8adbf565d5ff49720966e0919b0
KW <kw@KWnoMacBook-Pro.local>
2021-11-12 19:57:05 +0900

    system specのログインせずタスク一覧画面に飛ぼうとした場合、ログアウトした場合を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,14 +13,14 @@ class SessionsController < ApplicationController
         redirect_to tasks_path
       end
     else
-      flash.now[:danger] = 'ログインに失敗しました'
+      flash.now[:danger] = 'ログインに失敗しました。'
       render :new
     end
   end
 
   def destroy
     session.delete(:user_id)
-    flash[:notice] = 'ログアウトしました'
+    flash[:notice] = 'ログアウトしました。'
     redirect_to new_session_path
   end
 end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -11,41 +11,39 @@ RSpec.describe 'ユーザー機能', type: :system do
   end
 
   describe '新規作成機能' do
-    # context 'ユーザーを新規作成した場合' do
-    #   it '作成したユーザーが表示される' do
-    #     visit new_user_path
-    #     fill_in :user_name, with: 'test1234'
-    #     fill_in :user_email, with: 'test@test.com'
-    #     fill_in :user_password, with: 'test'
-    #     fill_in :user_password_confirmation, with: 'test'
-    #     click_button 'アカウントを作成する'
-    #     visit tasks_path
-    #     expect(page).to have_content 'test1234'
-    #   end
-    # end
+    context 'ユーザーを新規作成した場合' do
+      it '作成したユーザーが表示される' do
+        visit new_user_path
+        fill_in :user_name, with: 'test1234'
+        fill_in :user_email, with: 'test@test.com'
+        fill_in :user_password, with: 'test'
+        fill_in :user_password_confirmation, with: 'test'
+        click_button 'アカウントを作成する'
+        visit tasks_path
+        expect(page).to have_content 'test1234'
+      end
+    end
 
-    # context 'ログインせずタスク一覧画面に飛ぼうとした場合' do
-    #   it 'ログイン画面が表示される' do
-    #     visit tasks_path
-    #     byebug
-    #     click_on "Logout"
-    #     visit tasks_path
-    #     expect(response).to redirect_to new_session_path
-    #   end
-    # end
+    context 'ログインせずタスク一覧画面に飛ぼうとした場合' do
+      it 'ログイン画面が表示される' do
+        click_on "Logout"
+        visit tasks_path
+        expect(page).to have_content 'Sign up Login'
+      end
+    end
   end
 
   describe 'セッション機能' do
-  #   context 'ログインした場合' do
-  #     it 'ログインしたユーザーが表示される' do
-  #       visit new_session_path
-  #       fill_in :session_email, with: @user.email
-  #       fill_in :session_password, with: @user.password
-  #       click_button 'Log in'
-  #       expect(page).to have_content 'factory_user_aaa'
-  #     end
-  #   end
-  #
+    context 'ログインした場合' do
+      it 'ログインしたユーザーが表示される' do
+        visit new_session_path
+        fill_in :session_email, with: @user.email
+        fill_in :session_password, with: @user.password
+        click_button 'Log in'
+        expect(page).to have_content 'factory_user_aaa'
+      end
+    end
+
     context 'ユーザーの詳細画面に遷移した場合' do
       it '詳細画面が表示される' do
         visit user_path(@user.id)
@@ -53,10 +51,18 @@ RSpec.describe 'ユーザー機能', type: :system do
       end
     end
 
-    context '他の一般ユーザーの詳細画面に遷移した場合' do
+    context '一般ユーザーが他ユーザーの詳細画面に遷移した場合' do
       it '自分の一覧画面が表示される' do
         visit tasks_path(@user_2.id)
         expect(page).to have_content 'factory_user_aaa' && 'タスク一覧'
+      end
+    end
+
+    context 'ログアウトした場合' do
+      it 'ログアウトしました。が表示される' do
+        visit tasks_path
+        click_on "Logout"
+        expect(page).to have_content 'ログアウトしました。'
       end
     end
   end
